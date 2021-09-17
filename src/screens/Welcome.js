28a9e436c1f92631e32be03bf94ba9c84fe772f4
Shokhrukh2606 <shakhrukh7ielts@mail.ru>
2021-09-17 22:15:08 +0500

    some change

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import animate from './animate.svg'
 const Welcome = (props) => {
-  return (
-    <div className="container-fluid welcome">
-    	<div className="row">
-    		<div className="col-md-9">
-    			<h1>
-    				CORAL DRAW va AUTOCAD dasturlarini <br/>o'rganish darslariga xush kelibsiz
-    			</h1>
-    			<button className="btn btn-info" onClick={()=>props.setScreen(1)}>
+	return (
+		<div className="container-fluid welcome">
+			<div className="row animate">
+				<div className="col-md-9">
+					<h1>
+						Immitatsion modellar <br />asosida kompyuter grafikasi<br /> mavzularini o'rgatish dasturi
+					</h1>
+					{/* <button className="btn btn-info mb-3" onClick={()=>props.setScreen(1)}>
     				Nazariy darslar
     			</button>
 				<button className="btn btn-info mb-3" onClick={()=>props.setScreen(4)}>
